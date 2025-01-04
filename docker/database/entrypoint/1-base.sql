4f7fb032b2285ec200c28aab9d18062f16afaa85
mvysotsky <m.vysotskyi@gmail.com>
2025-01-05 00:15:12 +0200

    basic setup

---
+++ docker/database/entrypoint/1-base.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `ouro`.`users`
+(
+    `id`          INT(11)     NOT NULL AUTO_INCREMENT,
+    `username`    VARCHAR(50) NOT NULL COMMENT 'Telegram username',
+    `wallet`      VARCHAR(44) NULL COMMENT 'SOL wallet address',
+    `created_at`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `referred_by` INT(11)     NULL COMMENT 'User ID of the referrer',
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`referred_by`) REFERENCES `users` (`id`),
+    UNIQUE INDEX `username` (`username`)
+);
+
+CREATE TABLE `ouro`.`leaderboard`
+(
+    `id`         INT(11)   NOT NULL AUTO_INCREMENT,
+    `user_id`    INT(11)   NOT NULL,
+    `score`      INT(11)   NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
